d4a73f01f5e20a9303cb51b163ab28cb88eba5f3
Mr.YEH <zxc09267520@gmail.com>
2019-08-26 01:11:48 +0800

    add Ticket class write Tester class

--- src/main/java/com/train/Tester.java
+++ src/main/java/com/train/Tester.java
@@ -1,7 +1,10 @@
 package com.train;
 
+import java.util.Scanner;
+
 public class Tester {
     public static void main(String[] args) {
-
+        Ticket user=new Ticket();
+        user.showAll();
     }
 }
--- src/main/java/com/train/Ticket.java
+++ src/main/java/com/train/Ticket.java
@@ -1,6 +1,43 @@
 package com.train;
 
+import java.util.Scanner;
+
 public class Ticket {
+
+    int ticket,returnTicket,oneWayTicket;
+    int total=0;
+
+    public void getTicket(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter how much ticket you want: ");
+        ticket=sc.nextInt();
+        System.out.println("ticket : "+ticket);
+    }
+
+    public void getReturnTicket(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter how much returnTicket :");
+        returnTicket=sc.nextInt();
+        System.out.println("returnTicket :"+returnTicket);
+    }
+
+    public void getOneWayTicket(){
+        oneWayTicket=ticket-returnTicket;
+        System.out.println("oneWayTicket: "+oneWayTicket);
+    }
+
+    public void getPrice(){
+       int price=1000*oneWayTicket+1800*returnTicket;
+        System.out.println("price="+price);
+
+    }
+
+    public void showAll(){
+        getTicket();
+        getReturnTicket();
+        getOneWayTicket();
+        getPrice();
+    }
 
 
 
